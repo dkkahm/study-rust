5cd0fe6d8f73359888ca2582872d4da0d4a20f4c
dkkahm <dkkahm@gmail.com>
2024-05-26 16:34:59 +0900

    3.7

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,21 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subcribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub async fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subcribe))
     })
     .listen(listener)?
     .run();
