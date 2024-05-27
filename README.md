8134ef6f9305dab605c34db5f7a79f3f5e8242ba
감동기 <dk.kam@tg360tech.com>
2024-05-27 10:28:34 +0900

    4.5.9

---
+++ README.md
@@ -0,0 +1,18 @@
+### 입출력
+- 입력
+    - Path
+    - Query
+    - Body
+    - ...
+- 출력
+    - impl Responder
+        - HttpResponse
+
+### 설정 (configuration)
+
+### 상태
+
+### Database
+
+### Logging
+- log crate
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use crate::routes::health_check::health_check;
 use crate::routes::subscriptions::subcribe;
@@ -12,6 +13,7 @@ pub async fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subcribe))
             .app_data(db_pool.clone())
